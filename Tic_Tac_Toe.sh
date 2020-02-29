c34937247e4f6af73428ad301709b3d7405c02af
Your Name <you@example.com>
2020-02-29 17:47:28 +0530

    Added : Who play first after toss

--- Tic_Tac_Toe.sh
+++ Tic_Tac_Toe.sh
@@ -4,6 +4,7 @@ PLAYER2=""
 
 declare -A matrix
 number=1
+Toss=0;
 for ((index=0; index<3; index++))
 do
 	for ((index1=0; index1<3; index1++))
@@ -15,5 +16,12 @@ do
 done
 read -p "Enter a First player name : " PLAYER1
 read -p "Enter a Second player name : " PLAYER2
-printf "$PLAYER1 play with X \n$PLAYER2 play with 0"
+printf "$PLAYER1 play with X \n$PLAYER2 play with 0 \n"
 
+toss=$(( RANDOM%3 + 1))
+if [ $toss -eq 1 ]
+then
+	printf "play first  $PLAYER1 \n"
+else 
+	printf "play first $PLAYER2 \n" 
+fi
